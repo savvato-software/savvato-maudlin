d3e055f42ad16452dfebb0ac34e2c4128e470280
Johnathan James <haxwell@gmail.com>
2025-01-19 16:47:13 -0700

    optimize, added last-best-trials and use-existing-model params. Some code cleanup

--- maudlin_core/src/optimizing/optimize.py
+++ maudlin_core/src/optimizing/optimize.py
@@ -1,11 +1,13 @@
 from maudlin_core.src.lib.framework.maudlin_unit_config import get_current_unit_config
-from maudlin_core.src.lib.framework.maudlin import load_maudlin_data
+from maudlin_core.src.lib.framework.maudlin import load_maudlin_data, get_current_unit_name
 from maudlin_core.src.common.data_preparation_manager import DataPreparationManager
 from maudlin_core.src.model.adaptive_learning_rate import AdaptiveLearningRate
 from maudlin_core.src.model.early_stopping_top_n import EarlyStoppingTopN
 from maudlin_core.src.lib.framework.stage_functions.pre_optimization_function import execute_preoptimization_stage
 from maudlin_core.src.lib.framework.stage_functions.post_optimization_function import execute_postoptimization_stage
+from maudlin_core.src.lib.framework.maudlin import load_yaml_file, save_yaml_file
 from sklearn.metrics import accuracy_score, mean_absolute_error, roc_auc_score
+from functools import partial
 
 import os
 import optuna
@@ -13,21 +15,73 @@ import json
 import yaml
 import shutil
 import datetime
+import argparse
 
 def run_optimization():
     main()
 
-def main():
+def main(cli_args=None):
+    """Main entry point for the optimization script."""
+    # Parse CLI args
+    parser = argparse.ArgumentParser(description="Optimize a model using Optuna.")
+    parser.add_argument(
+        "--trials", "-t",
+        type=int,
+        help="Number of trials to run.",
+    )
+    parser.add_argument(
+        "optimization_run_path",
+        nargs='?',
+        default=None,
+        help="Path to the optimization run directory (optional)."
+    )
+    parser.add_argument(
+        "--use-existing-model", "-m",
+        action='store_true',
+        help="Use an existing model for training."
+    )
+    parser.add_argument(
+        "--use-last-best-trials", "-lbt",
+        action='store_true',
+        help="Use the last best trials to constrain a new optimization run."
+    )
+    args = parser.parse_args(cli_args) if cli_args else parser.parse_args()
+
     data_dir = None
 
-    config = get_current_unit_config()
+    config = get_current_unit_config(args.optimization_run_path)
     config['mode'] = 'optimize'
+    config['optimization']['n_trials'] = args.trials
+
+    rconfig = config.get('runtime')
+
+    if not rconfig:
+        rconfig = {}
+        config['runtime'] = rconfig
+
+    rconfig['use_existing_model'] = args.use_existing_model
+    rconfig['use_last_best_trials'] = args.use_last_best_trials
+
+    ## create a runtime section in the config, for runtime params.. hack, because we're saving the config, and this stuff is in there...
+
+    if (rconfig['use_existing_model']):
+        print("---- using EXISTING model")
+    elif (rconfig['use_last_best_trials']):
+        print("---- optimizing A NEW model using the last best trials")
+    else:
+        print("---- optimizing A NEW model from scratch")
+
     maudlin = load_maudlin_data()
 
     # Setup directories
-    data_dir, run_id, parent_run_id = initialize_optimization_run_directory(maudlin)
-    config['run_id'] = run_id
-    config['parent_run_id'] = parent_run_id
+    data_dir, unit_dir, run_id, parent_run_id = initialize_optimization_run_directory(maudlin, config)
+    rconfig['run_id'] = run_id
+    rconfig['parent_run_id'] = parent_run_id
+    rconfig['current_unit_name'] = get_current_unit_name(maudlin)
+
+    if ('base_optimization_run_id' not in rconfig) or (rconfig['use_last_best_trials']):
+        rconfig['base_optimization_run_id'] = rconfig['run_id']
+
     oconfig = config['optimization']
 
     best_trials = []
@@ -37,16 +91,23 @@ def main():
 
     X_train, y_train, X_test, y_test, X_val, y_val = data_preparation_manager.load_and_prepare_data()
 
-    execute_preoptimization_stage(config)
+    execute_preoptimization_stage(config, unit_dir)
 
-    def objective(trial):
-        # Feature masking (binary switches for each feature)
-        feature_mask = [trial.suggest_categorical(f'feature_{i}', [0, 1]) for i in range(X_train.shape[1])]
+    ctr = 0
 
-        # Apply masking
-        selected_features = [i for i in range(len(feature_mask)) if feature_mask[i] == 1]
-        X_train_masked = X_train[:, selected_features]
-        X_test_masked = X_test[:, selected_features]
+    def objective(trial, ctr, X_train, y_train, X_test, y_test, X_val, y_val):
+        ctr += 1
+
+        X_train_masked = X_train.copy()
+        X_test_masked = X_test.copy()
+        X_val_masked = X_val.copy()
+
+        if oconfig['use_feature_masking']:
+            feature_mask = [trial.suggest_categorical(f'feature_{i}', [0, 1]) for i in range(X_train_masked.shape[1])]
+            selected_features = [i for i in range(len(feature_mask)) if feature_mask[i] == 1]
+            X_train_masked = X_train_masked[:, selected_features]
+            X_test_masked = X_test_masked[:, selected_features]
+            X_val_masked = X_val_masked[:, selected_features]
 
         # """Configure training callbacks"""
         metrics_to_track = config.get("metrics", ["mae"])
@@ -78,16 +139,16 @@ def main():
         config['batch_size'] = trial.suggest_int('batch_size', oconfig['batch_size']['min'], oconfig['batch_size']['max'])
 
         print()
-        print("Trial Number: ", trial.number)
+        print("Trial Number: ", trial.number, " | ", oconfig['n_trials'] - trial.number, "   ", ctr, " trials remaining")
         print("Trial Params: ", trial.params)
         print()
 
         config['epochs'] = 20
-        model = data_preparation_manager.setup_model(X_train.shape[-1])
-        _, model = data_preparation_manager.train_model(callbacks, X_train, y_train, X_val, y_val)
+        model = data_preparation_manager.setup_model(X_train_masked.shape[-1])
+        _, model = data_preparation_manager.train_model(callbacks, X_train_masked, y_train, X_val_masked, y_val)
 
         # think this really is posttraining, not yet postoptimization
-        y_preds = execute_postoptimization_stage(config, data_dir, data_preparation_manager.model, X_train, y_train, X_test, y_test)
+        y_preds = execute_postoptimization_stage(config, data_dir, data_preparation_manager.model, X_train_masked, y_train, X_test_masked, y_test)
 
         # Evaluate based on the first metric listed in the config
         metrics = config['metrics']
@@ -114,8 +175,9 @@ def main():
         # Return the score for Optuna optimization
         return score
 
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=oconfig['n_trials'])
+    study = get_study_object(config) # optuna.create_study(direction='maximize')
+    objective_with_data = partial(objective, ctr=ctr, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, X_val=X_val, y_val=y_val)
+    study.optimize(objective_with_data, n_trials=oconfig['n_trials'])
 
     # Write best trials to a file
     best_trials_file = os.path.join(data_dir, "best_trials.yaml")
@@ -143,6 +205,27 @@ def main():
     print("Best Params: ", study.best_params)
     print()
     print("Finished at ", datetime.datetime.now())
+
+def get_study_object(config):
+    # Define the database file for SQLite
+    db_path = "optuna_study.db"
+    storage = f"sqlite:///{db_path}"
+
+    study = None
+
+    if config and config['runtime'] and config['runtime']['use_existing_model']:
+        # Load the study from the SQLite database
+        name = f"optuna_study_run_{config['runtime']['base_optimization_run_id']}"
+        print(f"Loading existing study: {name}")
+        study = optuna.load_study(study_name=name, storage=storage)
+    else:
+        # Create a study and connect it to the SQLite database
+        name = f"optuna_study_run_{config['runtime']['run_id']}"
+        print(f"Creating new study: {name}")
+        study = optuna.create_study(study_name=name, storage=storage, direction="maximize")
+
+    return study
+
 
 def update_config_with_suggested_model_architecture(trial, base_config):
     """
@@ -205,9 +288,10 @@ def update_config_with_suggested_model_architecture(trial, base_config):
 
     return base_config
 
-def initialize_optimization_run_directory(maudlin):
+## TODO: Move this to a common utility file, its in batch.py as well
+def initialize_optimization_run_directory(maudlin, config):
     # Define paths
-    unit_dir = os.path.join(maudlin['data-directory'], 'optimization', maudlin['current-unit'])
+    unit_dir = os.path.join(maudlin['data-directory'], 'optimizations', maudlin['current-unit'])
     os.makedirs(unit_dir, exist_ok=True)
 
     # Counter file path
@@ -234,10 +318,30 @@ def initialize_optimization_run_directory(maudlin):
     data_dir = os.path.join(unit_dir, f"run_{metadata['current_run_id']}")
     os.makedirs(data_dir, exist_ok=True)
 
-    config_path = os.path.join(maudlin['data-directory'], 'configs', maudlin['current-unit'] + ".config.yaml")
-    shutil.copy(config_path, data_dir + "/config.yaml")
+    # Copy config file to the current run directory
+    prev_config_path = ''
 
-    return data_dir, metadata['current_run_id'], prev_curr_run_id
+    if config and config.get('runtime') and config.get('runtime').get('use_existing_model'):
+        prev_config_path = os.path.join(unit_dir, f"run_{prev_curr_run_id}/config.yaml")
+    else:
+        prev_config_path = os.path.join(maudlin['data-directory'], 'configs', maudlin['current-unit'] + ".config.yaml")
+
+    if not os.path.exists(prev_config_path):
+        raise FileNotFoundError(f"Config file not found at {prev_config_path}")
+
+    shutil.copy(prev_config_path, data_dir + "/config.yaml")
+
+    # open the config file and update the run_id and parent_run_id
+    c = load_yaml_file(data_dir + "/config.yaml")
+
+    if 'runtime' not in c:
+        c['runtime'] = {}
+
+    c['runtime']['run_id'] = metadata['current_run_id']
+    c['runtime']['parent_run_id'] = prev_curr_run_id
+    save_yaml_file(c, data_dir + "/config.yaml")
+
+    return data_dir, unit_dir, metadata['current_run_id'], prev_curr_run_id
 
 
 if __name__ == "__main__":
